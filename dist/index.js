025b596368084a05b6a3f54683eacf32d338de95
Takahito Uchino <takahito_uchino@58x12x96x150.ap58.ftth.ucom.ne.jp>
2023-10-12 22:08:35 +0900

    4.5 練習問題

--- dist/index.js
+++ dist/index.js
@@ -1,10 +1,31 @@
 "use strict";
-const a = 1 + 2;
-const b = a + 3;
-const c = {
-    apple: a,
-    banana: b
+let reserve = (fromOrDestination, toOrDestination, destination) => {
+    if (fromOrDestination instanceof Date &&
+        toOrDestination instanceof Date &&
+        destination !== undefined) {
+        console.log("宿泊旅行を予約する");
+    }
+    else if (fromOrDestination instanceof Date &&
+        typeof toOrDestination === "string") {
+        console.log("日帰り旅行を予約する");
+    }
+    else if (typeof fromOrDestination === "string") {
+        console.log("すぐに出発する旅行を予約する");
+    }
 };
-const d = c.apple * 4;
-console.log(d);
+function call(f, ...args) {
+    return f(...args);
+}
+function fill(length, value) {
+    return Array.from({ length }, () => value);
+}
+call(fill, 10, "a");
+function is(a, ...b) {
+    return b.every(_ => _ === a);
+}
+is("string", "otherstring");
+is(true, false);
+is(42, 42);
+is(10, "foo");
+is([1], [1, 2], [1, 2, 3]);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,55 @@
-let a = 1042
-let b = "apples and orages"
-const c = "pineapples"
-let d = [true, true, false]
-let e = {type: "ficus"}
-let f = [1, false]
-const g = [3]
-let h = null
+type Reservation = unknown
+
+type Reserve = {
+  (from: Date, to: Date, destination: string): Reservation
+  (from: Date, destination: string): Reservation
+  (destination: string): Reservation
+}
+
+let reserve: Reserve = (
+  fromOrDestination: Date | string,
+  toOrDestination?: Date | string,
+  destination?: string
+) => {
+  if (
+    fromOrDestination instanceof Date &&
+    toOrDestination instanceof Date &&
+    destination !== undefined
+  ) {
+    console.log("宿泊旅行を予約する")
+  } else if (
+    fromOrDestination instanceof Date &&
+    typeof toOrDestination === "string"
+  ) {
+    console.log("日帰り旅行を予約する")
+  } else if (typeof fromOrDestination === "string") {
+    console.log("すぐに出発する旅行を予約する")
+  }
+}
+
+function call<T extends [unknown, string, ...unknown[]], R>(
+  f: (...args: T) => R,
+  ...args: T
+): R {
+  return f(...args)
+}
+
+function fill(length: number, value: string): string[] {
+  return Array.from({length}, () => value)
+}
+
+call(fill, 10, "a")
+
+function is<T>(a: T, ...b: [T, ...T[]]): boolean {
+  return b.every(_ => _ === a)
+}
+
+is("string", "otherstring")
+
+is(true, false)
+
+is(42, 42)
+
+is(10, "foo")
+
+is([1], [1, 2], [1, 2, 3])
