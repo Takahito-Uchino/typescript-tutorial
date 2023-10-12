b5d201ffda895c9aa8f7a6dabd98b7c225c5629a
Takahito Uchino <takahito_uchino@58x12x96x150.ap58.ftth.ucom.ne.jp>
2023-10-12 10:59:25 +0900

    2.5 練習問題

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,10 @@
 "use strict";
-console.log("Hello TypeScript!");
+const a = 1 + 2;
+const b = a + 3;
+const c = {
+    apple: a,
+    banana: b
+};
+const d = c.apple * 4;
+console.log(d);
 //# sourceMappingURL=index.js.map
--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,8 @@
-console.log("Hello TypeScript!")
+const a = 1 + 2
+const b = a + 3
+const c = {
+  apple: a,
+  banana: b
+}
+const d = c.apple * 4
+console.log(d)
