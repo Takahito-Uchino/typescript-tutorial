969626ff3c625423b2cfdf929dfabc34236f05f4
Takahito Uchino <takahito_uchino@58x12x96x150.ap58.ftth.ucom.ne.jp>
2023-10-12 10:53:24 +0900

    環境構築

---
+++ .eslintrc.js
@@ -0,0 +1,17 @@
+module.exports = {
+  root: true,
+  plugins: [
+    "@typescript-eslint"
+  ],
+  parser: "@typescript-eslint/parser",
+  parserOptions: {
+    project: "./tsconfig.json"
+  },
+  extends: [
+    "eslint:recommended",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier"
+  ]
+};
