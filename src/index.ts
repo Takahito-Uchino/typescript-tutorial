380efb695fe412b8ee31d3346bc6f37316850712
Takahito Uchino <takahito_uchino@58x12x96x150.ap58.ftth.ucom.ne.jp>
2023-10-14 12:06:37 +0900

    8.8 練習問題

--- src/index.ts
+++ src/index.ts
@@ -1,49 +1,115 @@
-type UserID = unknown
+import { readFile } from "fs"
+import { fork } from "child_process"
+import { resolve } from "path"
 
-declare class API {
-  getLoggedInUserID(): Option<UserID>
-  getFriendIDs(userID: UserID): Option<UserID[]>
-  getUserName(userID: UserID): Option<string>
+function promisify<T, A>(
+  f: (arg: A, f: (error: unknown, result: T | null) => void) => void
+): (arg: A) => Promise<T> {
+  return (arg: A) =>
+    new Promise<T>((resolve, reject) =>
+      f(arg, (error, result) => {
+        if (error) {
+          return reject(error)
+        }
+        if (result === null) {
+          return reject(null)
+        }
+        resolve(result)
+      })
+    )
 }
 
-interface Option<T> {
-  flatMap<U>(f: (value: T) => None): None
-  flatMap<U>(f: (value: T) => Option<U>): Option<U>
-  getOrElse(value: T): T
-}
+const readFilePromise = promisify(readFile)
+readFilePromise(__dirname + "/exercises.js")
+  .then(result => console.log("done!", result.toString()))
 
-class Some<T> implements Option<T> {
-  constructor(private value: T) {}
-  flatMap<U>(f: (value: T) => None): None
-  flatMap<U>(f: (value: T) => Some<U>): Some<U>
-  flatMap<U>(f: (value: T) => Option<U>): Option<U> {
-    return f(this.value)
-  }
-  getOrElse(): T {
-    return this.value
-  }
-}
+type Matrix = number[][]
 
-class None implements Option<never> {
-  flatMap(): None {
-    return this
+type MatrixProtocol = {
+  determinant: {
+    in: [Matrix]
+    out: number
   }
-  getOrElse<U>(value: U): U {
-    return value
+  "dot-product": {
+    in: [Matrix, Matrix]
+    out: Matrix
+  }
+  invert: {
+    in: [Matrix]
+    out: Matrix
   }
 }
 
-function listOfOptionsToOptionOfList<T>(list: Option<T>[]): Option<T[]> {
-  const empty = {}
-  const result = list.map(_ => _.getOrElse(empty as T)).filter(_ => _ !== empty)
-  if (result.length) {
-    return new Some(result)
+type Protocol = {
+  [command: string]: {
+    in: unknown[]
+    out: unknown
   }
-  return new None()
 }
 
-let api = new API()
-let friendsUserNames = api
-  .getLoggedInUserID()
-  .flatMap(api.getFriendIDs)
-  .flatMap(_ => listOfOptionsToOptionOfList(_.map(api.getUserName)))
+function createProtocol<P extends Protocol>(script: string) {
+  return <K extends keyof P>(command: K) => (...args: P[K]["in"]) =>
+    new Promise<P[K]["out"]>((resolve, reject) => {
+      let worker = new Worker(script)
+      worker.onerror = reject
+      worker.onmessage = event => resolve(event.data)
+      worker.postMessage({command, args})
+    })
+}
+
+let runWithMatrixProtocol = createProtocol<MatrixProtocol>(
+  "MatrixWorkerScript.js"
+)
+
+let parallelDeterminant = runWithMatrixProtocol("determinant")
+
+parallelDeterminant([[1, 2],[3, 4]]).then(
+  determinant => console.log(determinant)
+)
+
+type Data<
+  P extends Protocol,
+  C extends keyof P = keyof P
+> = C extends C
+  ? {command: C; args: P[C]["in"]}
+  : never
+
+function handle(
+  data: Data<MatrixProtocol>
+): MatrixProtocol[typeof data.command]["out"] {
+  switch (data.command) {
+    case "determinant":
+      return determinant(...data.args)
+    case "dot-product":
+      return dotProduct(...data.args)
+    case "invert":
+      return invert(...data.args)
+  }
+}
+
+onmessage = ({data}) => postMessage(handle(data))
+
+declare function determinant(matrix: Matrix): number
+declare function dotProduct(matrixA: Matrix, matrixB: Matrix): Matrix
+declare function invert(matrix: Matrix): Matrix
+
+function createProtocolCP<P extends Protocol>(script: string) {
+  return <K extends keyof P>(command: K) => (...args: P[K]["in"]) =>
+    new Promise<P[K]["out"]>((resolve, reject) => {
+      const child = fork(script)
+      child.on("error", reject)
+      child.on("message", resolve)
+      child.send({command, args})
+    })
+}
+
+const runWithMatrixProtocolCP = createProtocolCP<MatrixProtocol>(
+  "./ChildThread.js"
+)
+const parallelDeterminantCP = runWithMatrixProtocolCP("determinant")
+
+parallelDeterminantCP([[1, 2], [3, 4]]).then(
+  determinant => console.log(determinant)
+)
+
+process.on("message",data => process.send!(handle(data)))
